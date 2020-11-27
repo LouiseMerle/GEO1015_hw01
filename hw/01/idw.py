36014bf43b45e051cd001d9227e8cf3c0f0a4edd
Carolin Bachert <carolinbachert@gmail.com>
2020-11-27 17:06:09 +0100

    IDW update

--- hw/01/idw.py
+++ hw/01/idw.py
@@ -45,20 +45,77 @@ def main():
     radius = j_idw['radius']
     power = j_idw['power']
 
+
     x_list_points, y_list_points, z_list_points = split_xyz(list_pts_3d)
     
     zip_list = list(zip(x_list_points, y_list_points))
     tree = scipy.spatial.KDTree(zip_list)
 
-    ncols = math.ceil((max(x_list_points)-min(x_list_points) + (0.5 * cellsize))/cellsize) # (0.5 * cellsize) adds an additional col, even if max-min is divisible by cellsize
-    nrows = math.ceil((max(y_list_points)-min(y_list_points) + (0.5 * cellsize))/cellsize)
+    # calcalute number of rows and colums to wtrite in the asc file
+    ncols = math.ceil((max(x_list_points)-min(x_list_points))/cellsize)
+    nrows = math.ceil((max(y_list_points)-min(y_list_points))/cellsize)
     
-    range_y = range(int(min(y_list_points)), int(nrows * cellsize + cellsize), int(cellsize))
-    range_y1 = range(int(min(y_list_points)), int(max(y_list_points) + cellsize), int(cellsize))
-    range_x = range(int(min(x_list_points)), int(max(x_list_points) + cellsize), int(cellsize))
+    # make x and y ranges for the x and y axes for the bbox
+    # add 0.5 cellsize to find the centre points of the 
+    range_y = reversed(numpy.arange(min(y_list_points) + 0.5 * cellsize, max(y_list_points)+ 0.5 * cellsize, cellsize)) 
+    range_x = numpy.arange(min(x_list_points) + 0.5 * cellsize, max(x_list_points)+ 0.5 * cellsize, cellsize)
     
-    print(range_y)
-    print(range_y1)
+    # make list with all x y coordinates on the x and y axis of the raster
+    coordinate_lst = [[x, y] for y in range_y for x in range_x]
+    
+    def distance (p1, p2):
+        return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
+
+    def cellvalue_idw (point, r, p):               # point is centerpoint of cell
+        i_idw = tree.query_ball_point(point, r)        # i_idw list of indicies with points within radius
+
+        if len(i_idw) == 0:
+            return -9999
+
+        weight_list = []
+        for i in i_idw:
+            d = distance(point, zip_list[i])
+            w_i = 1/d ** p
+            weight_list.append(w_i)
+        
+        #if sum(weight_list) == 'inf':
+            #return -9999
+        
+        point_list = []
+        for i in i_idw:
+            point_list.append(z_list_points[i])
+        s = 0
+        for num1, num2 in zip(weight_list, point_list):
+            s += num1 * num2
+        
+        if s/sum(weight_list) == 'inf':
+            return -9999
+
+        return(s/sum(weight_list))
+        
+    raster_values = []
+
+    for i in coordinate_lst:
+        raster_values.append(cellvalue_idw(i, radius, power))
+        
+    row_nr = 0
+    col_nr = 0
+    with open('sample_idw.asc', 'w') as fh:
+        fh.writelines('NCOLS {}\n'.format(ncols))
+        fh.writelines('NROWS {}\n'.format(nrows))
+        fh.writelines('XLLCENTER {}\n'.format(min(x_list_points) + (0.5 * cellsize)))
+        fh.writelines('YLLCENTER {}\n'.format(min(y_list_points) + (0.5 * cellsize)))
+        fh.writelines('CELLSIZE {}\n'.format(j_idw['cellsize']))
+        fh.writelines('NO_DATA VALUE -9999\n')
+        
+        for i in raster_values:
+            fh.write(str(i)+' ')
+            col_nr += 1
+        
+            if col_nr == ncols:
+                col_nr = 0
+                row_nr += 1
+                fh.write('\n')
 
 if __name__ == '__main__':
     main()
--- hw/01/idw_test.py
+++ hw/01/idw_test.py
@@ -1,19 +1,61 @@
 import math
+import numpy
 
-cellsize = 3.0
-x_list_points = [8, 2]
-x_list_points = [8.8, 2.3]
+cellsize = 2.0
+x_list_points = [2.0, 3.8, 11.5, 8.9, 7.6, 6.1, 4.4, 10.7]
+y_list_points = [3.0, 7.7, 8.2, 5.6, 4.9, 3.1, 6.4, 5.8]
+z_list_points = [50, 40, 10, 20, 50, 20, 30, 10]
+
+id_list = [1,3,4,6,7]
+weight_list = [1, 0.5, 3, 0.25, 1]
+
+point_list = []
+for i in id_list:
+    point_list.append(z_list_points[i])
+
+
+s = 0
+for num1, num2 in zip(weight_list, point_list):
+	s += num1 * num2
+    
+print (s)
+#print(sum(products))
+
+
+
+
+
+
+
+
+
+
+
 
 ncols1 = math.ceil((max(x_list_points)-min(x_list_points))/cellsize)
 ncols = math.ceil((max(x_list_points)-min(x_list_points) + (0.5 * cellsize))/cellsize)
 print(ncols1)
 print(ncols)
 
-range_x1 = range(int(min(x_list_points)), int(ncols * cellsize + cellsize), int(cellsize))
+nrows1 = math.ceil((max(y_list_points)-min(y_list_points))/cellsize)
+nrows = math.ceil((max(y_list_points)-min(y_list_points) + (0.5 * cellsize))/cellsize)
 
-range_x = range(int(min(x_list_points)), int(max(x_list_points) + cellsize), int(cellsize))
+print(nrows1)
+print(nrows)
 
-print(range_x1)
-print(range_x)
+#range_x1 = range(int(min(x_list_points)), int(ncols * cellsize + cellsize), int(cellsize))
 
-    
+#range_x = range(int(min(x_list_points)), int(max(x_list_points) + cellsize), int(cellsize))
+#range_y = reversed(range(int(min(y_list_points)), int(max(y_list_points) + cellsize), int(cellsize)))
+
+
+# make x and y ranges for the x and y axes for the bbox
+# add 0.5 cellsize to find the centre points of the 
+range_y = reversed(numpy.arange(min(y_list_points) + 0.5 * cellsize, max(y_list_points)+ 0.5 * cellsize, cellsize)) 
+range_x = numpy.arange(min(x_list_points) + 0.5 * cellsize, max(x_list_points)+ 0.5 * cellsize, cellsize)
+
+#print(range_x1)
+#print(range_x)
+
+coordinate_lst = [[x, y] for y in range_y for x in range_x] # for ll of each cell or centerpoint??
+#print(coordinate_lst)
